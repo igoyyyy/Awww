9a7932ecd6e9b7b136534bbc364f5fe077f95a02
igoyyyy <80570953+igoyyyy@users.noreply.github.com>
2021-03-14 19:33:45 +0700

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -22,7 +22,7 @@ hoi {}, my name is {}! if you have any questions about how to use me please give
 
 im a group manager bot maintained by  [this person](tg://user?id={}).
 
-For more commands click /help...
+For more commands click /help....
 
 **Keep in mind that any changes you DO do to the source have to be on github, as per the license.**
 
